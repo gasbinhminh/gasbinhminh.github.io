9d27382dcdce1f91f1c62d824b8e779f192a6e61
Your Name <you@example.com>
2020-12-08 20:48:12 +0700

    add script change price

---
+++ settingPrice.js
@@ -0,0 +1,14 @@
+var prices =[
+    {
+        "id": "binh-gas-gia-dinh-do-12-kg",
+        "oldPrice": "",
+        "price": "370.000đ"
+    },
+    {
+        "id": "binh-gas-gia-dinh-xam-12-kg",
+        "oldPrice": "",
+        "price": "350.000đ"
+    }
+];
+
+console.log(document.querySelectorAll('.type-product'));
